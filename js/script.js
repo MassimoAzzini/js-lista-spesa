f08f479ab66d6309f8318179ff016336a4b2fc7a
MassimoAzzini <massimo.azzini.web@gmail.com>
2023-09-14 17:11:30 +0200

    [add]: JS consol.log ciclo while

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const listaSpesa = [
+  'banane',
+  'insalata',
+  'acqua',
+  'pomodori',
+  'mozzarella',
+  'yogurt',
+  'formaggio',
+  'carne',
+  'pesce',
+  'uova',
+]
+
+
+
+let i = 0;
+
+while(i < listaSpesa.length){
+
+  
+  const itemSpesa = listaSpesa[i];
+  
+  console.log(itemSpesa);
+
+
+  i++;
+}
+
